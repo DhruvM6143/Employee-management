5bbba8ebb1ae03038f59e2a9d3c30afe39b54401
Dhruv Madaan <madaandhruv50@gmail.com>
2024-11-10 23:12:14 +0530

    edited the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import { assets } from '../assets/frontend_assets/assets'
+import logo from '../assets/logo.jpg';
 const Footer = () => {
     return (
         <div>
             <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
                 <div>
-                    <img src="" className='mb-3 w-32' alt="" />
+                    <img src={logo} className='mb-3 w-[80px]' alt="" />
                     <p className='w-full md:w-2/3 text-gray-600'>
-                        Discover fashion that defines you. Shop our curated collections for timeless pieces, trendy must-haves, and everyday essentials—all designed with quality, style, and affordability in mind. Elevate your wardrobe today!
+                        A comprehensive employee management system enabling admin to create, update, and manage employee records. Features validation, image upload, Cloudinary integration, and secure database storage.
                     </p>
                 </div>
                 <div>
@@ -23,13 +24,13 @@ const Footer = () => {
                     <p className='text-cl font-medium mb-5'>GET IN TOUCH</p>
                     <ul className='flex flex-col gap-1 text-gray-600'>
                         <li>+1-212-456-7890</li>
-                        <li>contact@naruto.com</li>
+                        <li>contact@admin.com</li>
                     </ul>
                 </div>
             </div>
             <div className=''>
                 <hr />
-                <p className='py-5 text-sm text-center'>Copyright 2024@ naruto.com - All Right Reserved.</p>
+                <p className='py-5 text-sm text-center'>Copyright 2024@ (Dhruv Madaan) admin.com - All Right Reserved.</p>
             </div>
         </div>
     )
